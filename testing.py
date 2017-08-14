80b5bb14624a09d629de010a729d169e3e3121bb
sangbeom ma <aktkdqja12@gmail.com>
2017-08-14 17:58:01 +0900

    17.08.14 practice

--- testing.py
+++ testing.py
@@ -3,27 +3,29 @@ os.environ['TF_CPP_MIN_LOG_LEVEL']='2'
 import tensorflow as tf
 
 
-#
-# x_train = [1, 2, 3]
-# y_train = [1, 2, 3]
-#
-# W = tf.Variable(tf.random_normal([1]), name='weight')
-# b = tf.Variable(tf.random_normal([1]), name='bias')
-#
-# hypothesis = x_train * W + b
-#
-# cost = tf.reduce_mean(tf.square(hypothesis - y_train))
-#
-# optimizer = tf.train.GradientDescentOptimizer(learning_rate=0.01)
-#
-# train = optimizer.minimize(cost)
-#
-# sess = tf.Session()
-#
-# sess.run(tf.global_variables_initializer())
-#
-# for step in range(2001):
-#     sess.run(train)
-#     if step % 20 == 0:
-#         print(step, sess.run(cost), sess.run(W), sess.run(b))
+
+
+W = tf.Variable(tf.random_normal([1]), name='weight')
+b = tf.Variable(tf.random_normal([1]), name='bias')
+X = tf.placeholder(tf.float32, shape=[None])
+Y = tf.placeholder(tf.float32, shape=[None])
+
+hypothesis = X * W + b
+
+cost = tf.reduce_mean(tf.square(hypothesis - Y)) # 제곱 후 평균값 산출
+
+optimizer = tf.train.GradientDescentOptimizer(learning_rate=0.01)
+
+train = optimizer.minimize(cost)
+
+sess = tf.Session()
+
+sess.run(tf.global_variables_initializer())
+
+for step in range(2001):
+    cost_val, W_val, b_val,  _ = sess.run([cost, W, b, train],
+        feed_dict = {X: [1, 2, 3, 4, 5],
+                        Y: [2.1, 3.1, 4.1, 5.1, 6.1]})
+    if step % 20 == 0:
+        print(step, cost_val, W_val, b_val)
 
